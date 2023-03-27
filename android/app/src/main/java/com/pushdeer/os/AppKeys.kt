120fe9d67dbbe50fea0fdf197de6add2ae95ee75
xygengcn <xygengcn@qq.com>
2023-03-27 19:09:16 +0800

    fix: remove 移除不要的数据

--- android/app/src/main/java/com/pushdeer/os/AppKeys.kt
+++
@@ -1,9 +0,0 @@
-package com.pushdeer.os.values
-
-object AppKeys {
-
-    const val MiPush_Id = "2882303761520124028"
-    const val MiPush_Key = "5512012428028"
-
-    const val WX_Id = "wx3ae07931d0555a24"
-}
