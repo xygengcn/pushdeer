32368d628ff653d963779f193877b079f2db96b6
xygengcn <xygengcn@qq.com>
2023-04-09 16:29:59 +0800

    feat: 增加获取所有消息列表和用户数据

--- api/app/Http/Controllers/PushDeerAdminController.php
+++ api/app/Http/Controllers/PushDeerAdminController.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
-use App\Models\PushDeerUser;
-use App\Models\PushDeerKey;
-use App\Models\PushDeerDevice;
 use App\Models\PushDeerMessage;
 
 
@@ -15,6 +13,8 @@ class PushDeerAdminController extends Controller
 
     /**
      * 获取用户key
+     * 
+     * 参数:邮箱 账号
      */
     public function getUserPushKey(Request $request)
     {
@@ -22,15 +22,26 @@ class PushDeerAdminController extends Controller
         $validated = $request->validate(
             [
                 'account' => 'string|required',
+                'email' => 'string|nullable'
             ]
         );
+        $result = DB::select('SELECT k.`key`,k.`name` AS pushName,u.`name` AS account FROM push_deer_keys k JOIN push_deer_users u ON k.uid=u.id WHERE u.`name`= ? OR u.email= ? LIMIT 1;', [$validated['account'], $validated['email']]);
+        return http_result($result, 200);
     }
     /**
      * 拉取消息列表
      */
-    public function messageAll()
+    public function messageAll(Request $request)
     {
-        $pd_message = PushDeerMessage::select('*')->limit(10)->get();
-        return http_result(['list' => $pd_message], 200);
+        $validated = $request->validate(
+            [
+                'pageIndex' => 'integer|nullable',
+                'pageNum' => 'integer|nullable'
+            ]
+        );
+        $pageIndex = $validated['pageIndex'] - 1 > 0 ? $validated['pageIndex'] : 0;
+        $pageNum = $validated['pageNum'] || 15;
+        $result = DB::select('SELECT a.id,a.uid,u.NAME,a.TEXT,a.desp,a.type,a.created_at,a.updated_at FROM push_deer_messages a JOIN push_deer_users u ON a.uid=u.id ORDER BY a.updated_at DESC LIMIT ? OFFSET ?', [$pageNum, $pageIndex * $pageNum]);
+        return http_result($result, 200);
     }
 }
